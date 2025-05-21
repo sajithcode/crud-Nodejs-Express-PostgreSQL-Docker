0a747edc61bdf5b4de6cd4811c9cf62e38cb2fcc
Sajith Rathna Sampath <sajithsampath721@gmail.com>
2025-05-22 01:31:33 +0530

    change on index.js

--- src/index.js
+++ src/index.js
@@ -38,5 +38,5 @@ app.get('/', async (req, res) => {
 
 //Server running
 app.listen(PORT, () => {
-  console.log(`Server is running in http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
